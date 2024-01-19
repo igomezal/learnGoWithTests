ae96a15f89713e9ad07b3bec64ab5435e72194dd
igomezal <yvangomezalonso@gmail.com>
2024-01-19 14:49:49 +0100

    feat: include tests for maps

---
+++ dictionary/dictionary.go
@@ -0,0 +1,51 @@
+package dictionary
+
+const (
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exists")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+type Dictionary map[string]string
+
+func (d Dictionary) Add(word, definition string) error {
+	_, ok := d[word]
+
+	if ok {
+		return ErrWordExists
+	}
+
+	d[word] = definition
+	return nil
+}
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
+}
+
+func (d Dictionary) Update(word, definition string) error {
+	_, ok := d[word]
+
+	if !ok {
+		return ErrWordDoesNotExist
+	}
+
+	d[word] = definition
+	return nil
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
+}
