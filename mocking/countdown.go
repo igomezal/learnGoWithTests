6435a29debf29be18b7d03f64d87e6bfaa14a50b
igomezal <yvangomezalonso@gmail.com>
2024-01-23 15:20:02 +0100

    feat: implement test for mocking

---
+++ mocking/countdown.go
@@ -0,0 +1,37 @@
+package mocking
+
+import (
+	"fmt"
+	"io"
+	"time"
+)
+
+type Sleeper interface {
+	Sleep()
+}
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+	fmt.Fprint(out, finalWord)
+}
